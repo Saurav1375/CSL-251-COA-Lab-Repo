671ca3f2ad90185271fae9d8208c6db0a0925838
Saurav1375 <sauravgupta1375@gmail.com>
2025-01-22 16:55:54 +0530

    updated solutions

--- Lab-4/Quest1.cpp
+++ Lab-4/Quest1.cpp
@@ -3,51 +3,22 @@
 #include <string>
 using namespace std;
 
-vector<int> strToBinary(string& str) {
-    vector<int> ans;
-    for(char ch : str) ans.push_back(ch - '0');
-    return ans;
-}
+string AddSub(string x, string y, int len, bool sub) {
+    string res = "";
+    char c = sub ? '1' : '0';
 
-string binaryToStr(vector<int>& bin) {
-    string ans;
-    for(int b : bin) ans += to_string(b);
-    return ans;
-}
-
-vector<int> find2sComplement(vector<int>& num) {
-    vector<int> ans = num;
-    int n = ans.size();
-    bool is_one = false;
-    
-    for(int i = n-1; i >= 0; i--) {
-        if(!is_one) {
-            if(ans[i] == 1) is_one = true;
-            continue;
-        }
-        ans[i] = 1 - ans[i];
+    for (int i = 0; i < len; ++i) {
+        y[i] = ((y[i] - '0') ^ sub) + '0';
     }
-    return ans;
-}
 
-string AddSub(string& A, string& B, bool isAdd) {
-    vector<int> n1 = strToBinary(A);
-    vector<int> n2 = strToBinary(B);
-    int n = n1.size();
-    vector<int> ans(n);
-    
-    if(!isAdd) n2 = find2sComplement(n2);
-    
-    int carry = 0;
-    for(int i = n-1; i >= 0; i--) {
-        int sum = n1[i] + n2[i] + carry;
-        ans[i] = sum % 2;
-        carry = sum / 2;
+    for (int i = len - 1; i >= 0; --i) {
+        int tmp = (x[i] - '0') + (y[i] - '0') + (c - '0');
+        res = (char)((tmp % 2) + '0') + res;
+        c = (char)((tmp / 2) + '0');
     }
-    
-    return binaryToStr(ans);
-}
 
+    return res;
+}
 int main() {
     string A = "";
     cout << "Enter A: ";
@@ -56,8 +27,9 @@ int main() {
     cout << "Enter B: ";
     cin >> B;
 
-    cout << "A + B = " << AddSub(A, B, true) << endl;
-    cout << "A - B = " << AddSub(A, B, false) << endl;
+    int n = A.length();
+    cout << "A + B = " << AddSub(A, B, n, false) << endl;
+    cout << "A - B = " << AddSub(A, B,n,  true) << endl;
     
     return 0;
 }
